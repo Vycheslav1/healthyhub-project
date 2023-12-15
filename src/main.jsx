3f11b1224979fc29d4a436357a4cdd22889a6782
Rummut <kzantip@gmail.com>
2023-12-15 21:56:31 +0200

    ass

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; 
+import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,5 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter basename="/react_vite">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
---
+++ src/pages/RecomendedFood/RecomendedFood.jsx
@@ -0,0 +1,13 @@
+const RecommendedFood = () => {
+  return (
+    <>
+      <h1>Recommended food</h1>
+      <div>
+        <img src={'../../Images/ketogenic-diet.png'} alt="ketogenic-diet" />
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+};
