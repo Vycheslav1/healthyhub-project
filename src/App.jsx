cab05fde08221e8350505061fcc40b410b34985d
Rummut <kzantip@gmail.com>
2023-12-17 12:07:23 +0200

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-// import SharedLayout from 'components/SharedLayout/SharedLayout';
-// import FirstPage from 'pages/FirstPage/FirstPage';
-// import SecondPage from 'pages/SecondPage/SecondPage';
-// import HalfPage from 'pages/HalfPage/HalfPage';
-// import ErrorPage from 'pages/ErrorPage/ErrorPage';
 import { MainPage } from '../src/pages/MainPage';
 import { SingUpPage } from '../src/pages/SingUpPage';
 import { SingInPage } from '../src/pages/SingInPage';
@@ -18,38 +13,28 @@ import { RecommendedFood } from './pages/RecomendedFood/RecomendedFood';
 function App() {
   // console.log(test);
   return (
-    <AppWrapper>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route
-          path="/signup"
-          element={
-            <RestrictedRoude redirectTo="/dairy" component={<SingUpPage />} />
-          }
-        />
-        // <Route path="/signup" element={<SingUpPage />} />
-        <Route
-          path="/signin"
-          element={
-            <RestrictedRoude redirectTo="/dairy" component={<SingInPage />} />
-          }
-        />
-        // <Route path="/signin" element={<SingInPage />} />
-        <Route
-          path="/dairy"
-          element={<PrivateRoute redirectTo="/signin" component={<Dairy />} />}
-        />
-        // <Route path="/dairy" element={<Dairy />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        {/* <Route path="/" element={<SharedLayout />}>
-          <Route path="/first" element={<FirstPage />} />
-          <Route path="/second" element={<SecondPage />}>
-            <Route path=":half" element={<HalfPage />} />
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
-        </Route> */}
-      </Routes>
-    </AppWrapper>
+    <Routes>
+      <Route></Route>
+      <Route path="/" element={<OnePage />} />
+      <Route
+        path="/signup"
+        element={
+          <RestrictedRoude redirectTo="/dairy" component={<SingUpPage />} />
+        }
+      />
+      <Route
+        path="/signin"
+        element={
+          <RestrictedRoude redirectTo="/main" component={<SingInPage />} />
+        }
+      />
+      <Route
+        path="/main"
+        element={<PrivateRoute redirectTo="/signin" component={<MainPage />} />}
+      />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/recommended-food" element={<RecommendedFood />} />
+    </Routes>
   );
 }
 export default App;
