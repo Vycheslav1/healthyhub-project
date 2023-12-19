585ba8f8ba4320ea5aa79f30de33fe6618c9a8bc
Vyacheslav Stetsenko <slavastetsenko@gmail.com>
2023-12-19 12:16:27 +0200

    files

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -16,7 +16,7 @@ import {
   FootLinkTextTwo,
   FootLinkTwo,
 } from './SignInForm.styled';
-import one from '../../images/one.png';
+//import one from 'images/one.png';
 
 export const SignInForm = () => {
   const dispatch = useDispatch();
@@ -34,7 +34,7 @@ export const SignInForm = () => {
   return (
     <SignInContainer>
       <div>
-        <ImageSignIn src={one} alt="Sport and fitness tracker" />
+        <ImageSignIn src={'images/one.png'} alt="Sport and fitness tracker" />
       </div>
       <SignInFormWrapper>
         <SignInTitle>Sign in</SignInTitle>
