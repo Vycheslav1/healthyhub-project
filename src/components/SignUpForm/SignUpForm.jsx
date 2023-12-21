04dad869aaddb16e946f355b418bf027751e503e
Rummut <kzantip@gmail.com>
2023-12-21 22:59:30 +0200

    edits

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
