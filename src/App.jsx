19250e9a6c12e15293ca3334ed4b907538b2aa73
Rummut <kzantip@gmail.com>
2023-12-23 15:33:34 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-import FirstPage from 'pages/FirstPage/FirstPage';
-import SecondPage from 'pages/SecondPage/SecondPage';
-import HalfPage from 'pages/HalfPage/HalfPage';
-import ErrorPage from 'pages/ErrorPage/ErrorPage';
-import { AppWrapper } from './AppStyled';
 import { MainPage } from 'pages/MainPage';
 import { SingUpPage } from 'pages/SingUpPage';
 import { SingInPage } from 'pages/SingInPage';
 import { ForgotPasswordPage } from 'pages/ForgotPasswordPage';
 import { RecommendedFood } from 'pages/RecommendedFood/RecommendedFood';
-import { RestrictedRoude } from 'components/RestrictedRoude';
-import { PrivateRoute } from 'components/PrivateRoute';
+
 import { OnePage } from 'pages/OnePage';
 import { useEffect } from 'react';
 import { refreshUser } from './redux/auth/operations';
 import { useAuth } from 'src/hooks/useAuth';
+import { PrivateRoute } from './components/PriveteRoute/PrivateRoute';
+import { SettingPage } from './pages/SettingPage/SettingPage';
+import { DiaryPage } from './pages/DiaryPage/DiaryPage';
+import { Dashboard } from './components/Dashboard/Dashboard';
+import { RestrictedRoute } from './components/RestrictedRoute/RestrictedRoude';
 
 function App() {
   const dispatch = useDispatch();
@@ -27,37 +26,66 @@ function App() {
     dispatch(refreshUser());
   }, [dispatch]);
 
+  // const { isLoggedIn } = useAuth();
+
   return isRefreshing ? (
     <b>Refreshing user...</b>
   ) : (
-    <AppWrapper>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route path="/" element={<OnePage />} />
-          <Route
-            path="/signup"
-            element={
-              <RestrictedRoude redirectTo="/main" component={<SingUpPage />} />
-            }
-          />
-          <Route
-            path="/signin"
-            element={
-              <RestrictedRoude redirectTo="/main" component={<SingInPage />} />
-            }
-          />
-          <Route
-            path="/main"
-            element={
-              <PrivateRoute redirectTo="/signin" component={<MainPage />} />
-            }
-          />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-
-          <Route path="/recommended-page" element={<RecommendedFood />} />
-        </Route>
-      </Routes>
-    </AppWrapper>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<OnePage />} />
+        <Route
+          path="signup"
+          element={
+            <RestrictedRoute redirectTo="/" component={<SingUpPage />} />
+          }
+        />
+        <Route
+          path="signin"
+          element={
+            <RestrictedRoute redirectTo="/" component={<SingInPage />} />
+          }
+        />
+        <Route
+          path="main"
+          element={<PrivateRoute redirectTo="/" component={<MainPage />} />}
+        />
+        <Route
+          path="forgot-password"
+          element={
+            <RestrictedRoute
+              redirectTo="/"
+              component={<ForgotPasswordPage />}
+            />
+          }
+        />
+        <Route
+          path="dashboard"
+          element={
+            <PrivateRoute redirectTo="/signin" component={<Dashboard />} />
+          }
+        />
+      </Route>
+      <Route
+        path="diary"
+        element={
+          <PrivateRoute redirectTo="/signin" component={<DiaryPage />} />
+        }
+      />
+      <Route
+        path="recommended-food"
+        element={
+          <PrivateRoute redirectTo="/signin" component={<RecommendedFood />} />
+        }
+      />
+      <Route
+        path="settings"
+        elements={
+          <PrivateRoute redirectTo="/signin" component={<SettingPage />} />
+        }
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
